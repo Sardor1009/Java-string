2c676a0871e6aa0613b28c9513884c881eb3310b
Sardor1009 <islomovsardor1009@gmail.com>
2023-12-17 18:30:44 +0500

    frist commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//  pr1
+
+// function firstNletters(n){
+//     let letters = ""
+//     for (let i= 65; i <=65 + n; i++){
+//         res = String.fromCharCode(i) + " "
+//         letters += res
+//     }
+//     return letters
+// }
+// let N = firstNletters(20);
+// console.log(N)
+
+
+
+// pr2
+
